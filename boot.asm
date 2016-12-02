a6291ca92a0ee05ef1c5c0f5cee212472df0adc4
julceswhat <julceswhat@gmail.com>
2016-12-01 20:51:36 -0800

    This is our Beta relief. :)

---
+++ boot.asm
@@ -0,0 +1,98 @@
+; CpS 230 Lab 9: Julio Cesar Whatley (Jwhat331)
+;---------------------------------------------------
+; Bootloader that loads/runs a single-sector payload
+; program from the boot disk.
+;---------------------------------------------------
+bits 16
+
+; Our bootloader is 512 raw bytes: we treat it all as code, although
+; it has data mixed into it, too.
+section	.text
+
+; The BIOS will load us into memory at 0000:7C00h; NASM needs
+; to know this so it can generate correct absolute data references.
+org	0x7C00
+
+; First instruction: jump over initial data and start executing code
+start:	jmp	main
+
+; Embedded data
+boot_msg	db	"CpS 230 Bootloading Lab", 13, 10
+		db	"by Julio Cesar Whatley", 13, 10, 0
+boot_disk	db	0		; Variable to store the number of the disk we boot from
+retry_msg	db	"Error reading payload from disk; retrying...", 13, 10, 0
+
+;Gets the pressed key 
+GetPressedKey:
+    mov ah, 0
+    int 0x16  ;BIOS Keyboard Service 
+    ret 
+
+main:
+	; Set DS == CS (so data addressing is normal/easy)
+	mov ax, cs
+	mov ds, ax
+	; Save the boot disk number (we get it in register DL)
+	mov byte[boot_disk], dl
+	; Set SS == 0x0800 (which will be the segment we load everything into later)
+	mov ax, 0x0800
+	mov ss, ax
+	; Set SP == 0x0000 (stack pointer starts at the TOP of segment; first push decrements by 2, to 0xFFFE)
+	mov sp, 0x0000
+	; Print the boot message/banner
+	mov	dx, boot_msg
+	call	puts
+	call 	GetPressedKey
+	; use BIOS raw disk I/O to load sector 2 from disk number <boot_disk> into memory at 0800:0000h (retry on failure)
+	mov ax, 0x0202
+	mov ch, 0
+	mov cl, 2
+	mov dh, 0
+	mov dl, byte[boot_disk]
+	mov bx, 0x0800
+	mov es, bx
+	mov bx, 0x0000
+	int    0x13
+	;compare carry flag to 0 for success or failure
+	jnc .success
+	mov dx, retry_msg
+	call puts
+	ret
+.success:
+	; Finally, jump to address 0800h:0000h (sets CS == 0x0800 and IP == 0x0000)
+	jmp	0x0800:0x0000
+
+; print NUL-terminated string from DS:DX to screen using BIOS (INT 10h)
+; takes NUL-terminated string pointed to by DS:DX
+; clobbers nothing
+; returns nothing
+puts:
+	push	ax
+	push	cx
+	push	si
+	
+	mov	ah, 0x0e
+	mov	cx, 1		; no repetition of chars
+	
+	mov	si, dx
+.loop:	mov	al, [si]
+	inc	si
+	cmp	al, 0
+	jz	.end
+	int	0x10
+	jmp	.loop
+.end:
+	pop	si
+	pop	cx
+	pop	ax
+	ret
+
+; NASM mumbo-jumbo to make sure the boot sector signature starts 510 bytes from our origin
+; (logic: subtract the START_ADDRESS_OF_OUR_SECTION [$$] from the CURRENT_ADDRESS [$],
+;	yielding the number of bytes of code/data in the section SO FAR; then subtract
+;	this size from 510 to give us BYTES_OF_PADDING_NEEDED; finally, emit
+;	BYTES_OF_PADDING_NEEDED zeros to pad out the section to 510 bytes)
+	times	510 - ($ - $$)	db	0
+
+; MAGIC BOOT SECTOR SIGNATURE (*must* be the last 2 bytes of the 512 byte boot sector)
+	dw	0xaa55
--- kernel.asm
+++ kernel.asm
@@ -9,18 +9,20 @@ bits 16
 ; This tells NASM to generate addresses +256 bytes
 ; into our memory segment (which is how DOS will load
 ; us into memory).
-org	0x100
+org	0
 
 stack_A	TIMES 256 db 0
 stack_B	TIMES 256 db 0
 
 section	.text
 start:
+
+	mov	ax, cs
+	mov	ds, ax
+
 	; Print programmer information
-	mov ah, 0x09
 	mov dx, info
-	int 0x21
-	
+	call puts	
 	
 	mov sp, stack_A + stack_size
 	push task_A
@@ -39,19 +41,16 @@ start:
 	popf
 	
 task_B:
-	mov ah, 0x09
 	mov dx, msg_B
-	int 0x21
+	call puts
 	call yield
 	jmp task_B
 	
 task_A:
-	mov ah, 0x09
 	mov dx, msg_A
-	int 0x21
+	call puts
 	call yield
 	jmp task_A
-	
 	
 	
 
@@ -65,9 +64,35 @@ yield:
 	popf 
 	ret
 	
+; print NUL-terminated string from DS:DX to screen using BIOS (INT 10h)
+; takes NUL-terminated string pointed to by DS:DX
+; clobbers nothing
+; returns nothing
+puts:
+	push	ax
+	push	cx
+	push	si
+	
+	mov	ah, 0x0e
+	mov	cx, 1		; no repetition of chars
+	
+	mov	si, dx
+.loop:	mov	al, [si]
+	inc	si
+	cmp	al, 0
+	jz	.end
+	int	0x10
+	jmp	.loop
+.end:
+	pop	si
+	pop	cx
+	pop	ax
+	ret
+	
+	
 section	.data
 stack_size equ 256
 saved_sp dw 0
-msg_A db 13, 10, "task one", 13, 10, "$"
-msg_B db 13, 10, "task two", 13, 10, "$"
-info db 13, 10, "CpS 230 Lab 4: Julio C W. College-Student (jwhat331)", 13, 10, "$"
+msg_A db 13, 10, "task one", 13, 10, 0
+msg_B db 13, 10, "task two", 13, 10, 0
+info db 13, 10, "CpS 230 Lab 4: Julio C W. College-Student (jwhat331)", 13, 10, 0
